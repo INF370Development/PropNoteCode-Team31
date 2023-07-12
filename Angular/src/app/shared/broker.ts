5d88d8dd0796b1cd247e21f1dc85d2eb65e9756f
u05038015 <102801806+u05038015@users.noreply.github.com>
2023-07-12 13:42:26 +0200

    Ciara

--- Angular/src/app/shared/broker.ts
+++
@@ -1,9 +0,0 @@
-export interface Broker {
-  brokerID: Number;
-  name: String;
-  surname : String;
-  phoneNumber: String;
-  officeAddress: String;
-  licenseNumber: String;
-  commissionRate: String;
-}
