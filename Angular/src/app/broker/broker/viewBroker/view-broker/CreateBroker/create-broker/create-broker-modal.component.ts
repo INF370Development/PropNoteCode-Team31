02f04b52137f103f66c724a6634f677d95dc3a56
Pippa Jarvie <u19046333@tuks.co.za>
2023-07-30 21:02:44 +0200

    IDFK

--- Angular/src/app/broker/broker/viewBroker/view-broker/CreateBroker/create-broker/create-broker-modal.component.ts
+++ Angular/src/app/broker/broker/viewBroker/view-broker/CreateBroker/create-broker/create-broker-modal.component.ts
@@ -1,21 +1,5 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
-NgModule({
-  imports: [
-    BrowserAnimationsModule,
-    BrowserModule,
-    FormsModule,
-    MatFormFieldModule,
-  ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-});
 
 @Component({
   selector: 'app-create-broker-modal',
