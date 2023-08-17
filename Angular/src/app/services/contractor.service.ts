8fdb9c4616bbbeb3598cce4063fb2e9eb6c02524
u05038015 <102801806+u05038015@users.noreply.github.com>
2023-08-17 20:14:35 +0200

    Contractor Services

---
+++ Angular/src/app/services/contractor.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Broker } from '../shared/Broker';
+import { map, Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BrokerService {
+  apiUrl = 'http://localhost:7251/api/';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      ContentType: 'application/json',
+    }),
+  };
+
+  constructor(private _httpClient: HttpClient) {}
+
+  getBrokers(): Observable<Broker[]> {
+    return this._httpClient
+      .get<Broker[]>(`https://localhost:7251/api/Broker/GetAllBrokers`)
+      .pipe(map((result) => result));
+  }
+
+  getBroker(brokerID: number, broker: Broker) {
+    return this._httpClient
+      .post(
+        `https://localhost:7251/api/Broker/GetBrokerByID/${brokerID}`,
+        broker
+      )
+      .pipe(map((result) => result));
+  }
+
+  deleteBroker(brokerID: number) {
+    return this._httpClient
+      .delete(`https://localhost:7251/api/Broker/DeleteBroker/?propertyID=${brokerID}`)
+      .pipe(map((result) => result));
+  }
+
+  createBroker(broker: Broker) {
+    return this._httpClient
+      .post(`https://localhost:7251/api/Broker/AddBroker`, broker)
+      .pipe(map((result) => result));
+  }
+}
---
+++ Angular/src/app/shared/Contractor.ts
@@ -0,0 +1,9 @@
+export interface Contractor {
+    id : number;
+    email : string;
+    name : string;
+    surname : string ;
+    phone : string ;
+    jobTitle : string;
+}
+  
