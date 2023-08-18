211e9c0e4580a811947788d4b2a31b246715b7d0
u19046333 <u19046333@tuks.co.za>
2023-08-18 17:09:50 +0200

    Fixing some stuff and starting on property images

--- WebApi/Models/Property/Property.cs
+++ WebApi/Models/Property/Property.cs
@@ -8,10 +8,25 @@
         public string Street { get; set; }
         public string Suburb { get; set; }
         public int PurchaseAmount { get; set; }
-        public string PurchaseYear { get; set; }
+        public int PurchaseYear { get; set; }
         public string Size { get; set; }
         public string Yard { get; set; }
         public int BrokerID { get; set; }
         public WebApi.Models.Broker.Broker Broker { get; set; }
+        
     }
+
+    public class PropertyImage
+    {
+        public int PropertyImageId { get; set; } // Primary key
+
+        // Other properties
+        public string ImageName { get; set; }
+        public byte[] ImageData { get; set; }
+
+        // Foreign key
+        public int PropertyId { get; set; }
+        public Property Property { get; set; }
+    }
+
 }
