ada2978bbd32a931e7cce11d28efd13162daf79a
u05038015 <102801806+u05038015@users.noreply.github.com>
2023-08-19 21:23:30 +0200

    Ciara API Try - 7 errors

--- WebApi/Controllers/TenantController.cs
+++ WebApi/Controllers/TenantController.cs
@@ -5,15 +5,164 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Reflection.Metadata.Ecma335;
 using System.Xml.Linq;
+using WebApi.Models.Users;
 using WebApi.Models.Interfaces;
+using WebApi.Repositories;
 
 namespace WebApi.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class TenantController : Controller
     {
-        public IActionResult Index()
+        private readonly ITenantRepository _tenantRepository;
+
+        public TenantController(ITenantRepository repository)
         {
-            return View();
+            _tenantRepository = repository;
+        }
+
+        [HttpGet]
+        [Route("GetAllTenants")]
+        public async Task<IActionResult> GetAllTenants()
+        {
+            try
+            {
+                List<Tenant> tenant = new();
+                var results = await _tenantRepository.GetAllTenantsAsync();
+                foreach (var tenant in results)
+                {
+                    tenant.Add(new Tenant
+                    {
+                        TenantID = tenant.TenantID,
+                        CompanyName = tenant.CompanyName,
+                        CompanyNumber = tenant.CompanyNumber,
+                    });
+                }
+                return Ok(tenant);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error, please contact support");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetTenantByID/{tenantID}")]
+        public async Task<IActionResult> GetTenantID(int tenantID)
+        {
+            try
+            {
+                var result = await _tenantRepository.GetTenantByID(tenantID);
+
+                if (result == null) return NotFound("Tenant does not exist. You need to create it first");
+
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Internal Server Error. Please contact support");
+            }
+        }
+
+        [HttpPost]
+        [Route("AddTenant")]
+        public async Task<IActionResult> AddTenant(TenantViewModel tenantModel)
+        {
+            var tenant = new Tenant
+            {
+                Email = tenantModel.Email,
+                FirstName = tenantModel.FirstName,
+                Surname = tenantModel.Surname,
+                JobTitle = tenantModel.JobTitle,
+            };
+            try
+            {
+                await _tenantRepository.AddTenant(tenant);
+
+                return Ok(tenant);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Internal Server Error. Please contact support.");
+            }
+        }
+
+        [HttpPut]
+        [Route("EditTenant")]
+        public async Task<IActionResult> EditTenant(int tenantID, TenantViewModel tenantModel)
+        {
+            try
+            {
+                var allTenants = await _tenantRepository.GetAllTenantsAsync();
+                var existingTenant = allTenants.FirstOrDefault(x => x.TenantID == tenantID);
+                if (existingTenant == null) return NotFound($"The tenant does not exist");
+
+                if (tenantModel.Email == "")
+                {
+                    existingTenant.Email = existingTenant.Email;
+                }
+                else
+                {
+                    existingTenant.Email = tenantModel.Email;
+                }
+                if (tenantModel.FirstName == "")
+                {
+                    existingTenant.FirstName = existingTenant.FirstName;
+                }
+                else
+                {
+                    existingTenant.FirstName = tenantModel.FirstName;
+                }
+                if (tenantModel.Surname == "")
+                {
+                    existingTenant.Surname = tenantModel.Surname;
+                }
+                else
+                {
+                    existingTenant.Surname = tenantModel.Surname;
+                }
+                if (tenantModel.JobTitle == "")
+                {
+                    existingTenant.JobTitle = tenantModel.JobTitle;
+                }
+                else
+                {
+                    existingTenant.JobTitle = tenantModel.JobTitle;
+                }
+
+                if (await _tenantRepository.SaveChangesAsync() == true)
+                {
+                    return Ok(existingTenant);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Internal Server Error. Please contact support.");
+            }
+            return BadRequest("Your request is invalid");
+        }
+
+        [HttpDelete]
+        [Route("DeleteTenant")]
+        public async Task<IActionResult> DeleteTenant(int tenantID)
+        {
+            try
+            {
+                var allTenants = await _tenantRepository.GetAllTenantsAsync();
+                var existingTenant = allTenants.FirstOrDefault(x => x.TenantID == tenantID);
+
+                if (existingTenant == null) return NotFound($"The Tenant does not exist");
+
+                _tenantRepository.Delete(existingTenant);
+
+                if (await _tenantRepository.SaveChangesAsync()) return Ok(existingTenant);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Internal Server Error. Please contact support.");
+            }
+            return BadRequest("Your request is invalid.");
         }
     }
 }
--- WebApi/ViewModels/TenantViewModel.cs
+++ WebApi/ViewModels/TenantViewModel.cs
@@ -3,7 +3,7 @@
     public class TenantViewModel
     {
         public int TenantID { get; set; }
-        public int Email { get; set; }
+        public string Email { get; set; }
         public string FirstName { get; set; }
         public string Surname { get; set; }
         public string JobTitle { get; set; }
