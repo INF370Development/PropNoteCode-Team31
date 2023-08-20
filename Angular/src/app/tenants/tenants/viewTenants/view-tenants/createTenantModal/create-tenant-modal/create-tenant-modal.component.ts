c8ff8aa322853a786d2def10be1f9b24a328f749
u05038015 <102801806+u05038015@users.noreply.github.com>
2023-08-20 23:33:26 +0200

    Tenant Validations on Create

--- Angular/src/app/tenants/tenants/viewTenants/view-tenants/createTenantModal/create-tenant-modal/create-tenant-modal.component.ts
+++ Angular/src/app/tenants/tenants/viewTenants/view-tenants/createTenantModal/create-tenant-modal/create-tenant-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EventEmitter, Output  } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { TenantService } from 'src/app/services/tenant.service';
 import { UserService } from 'src/app/services/user.service';
@@ -19,6 +19,8 @@ export class CreateTenantModalComponent implements OnInit {
   editorRole: boolean = false;
   viewerRole: boolean = false;
 
+  hide = true;
+
   tenantModel: UserTenant = {
     username: "",
     password : "",
@@ -26,9 +28,9 @@ export class CreateTenantModalComponent implements OnInit {
     name : "",
     surname : "",
     phoneNumber : "",
-  companyName : "",
-  companyNumber : "",
-  profilePhoto : "",
+    companyName : "",
+    companyNumber : "",
+    profilePhoto : "",
   }
 
   constructor(
@@ -42,11 +44,10 @@ export class CreateTenantModalComponent implements OnInit {
   ngOnInit(): void {  }
 
   CreateTenant() {
-    debugger;
+    //debugger;
     this.tenantService.createTenant(this.tenantModel).subscribe(
       (response) => {
         console.log('Tenant created successfully:', response);
-        // You can optionally close the modal after creating the broker
         this.dialogRef.close();
         location.reload();
       },
@@ -60,5 +61,86 @@ export class CreateTenantModalComponent implements OnInit {
 
   closeModal() {
     this.dialogRef.close();
+  }
+
+  //Username
+  username = new FormControl('', [Validators.required]);
+
+  getErrorMessageUsername() {
+    if (this.username.hasError('required')) {
+      return 'Username required';
+    }
+
+    return this.username.hasError('username') ? 'Not a valid username' : '';
+  }
+  //Password
+  password = new FormControl('', [Validators.required]);
+
+  getErrorMessagePassword() {
+    if (this.password.hasError('required')) {
+      return 'Password required';
+    }
+
+    return this.password.hasError('password') ? 'Not a valid password' : '';
+  }
+  //Email
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessageEmail() {
+    if (this.email.hasError('required')) {
+      return 'Email required';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  //Name
+  name = new FormControl('', [Validators.required]);
+
+  getErrorMessageName() {
+    if (this.name.hasError('required')) {
+      return 'Name required';
+    }
+
+    return this.email.hasError('name') ? 'Not a valid name' : '';
+  }
+  //Surname
+  surname = new FormControl('', [Validators.required]);
+
+  getErrorMessageSurname() {
+    if (this.surname.hasError('required')) {
+      return 'Surname required';
+    }
+
+    return this.surname.hasError('surname') ? 'Not a valid surname' : '';
+  }
+  //Phone Number
+  phoneNumber = new FormControl('', [Validators.required]);
+
+  getErrorMessagePhoneNumber() {
+    if (this.phoneNumber.hasError('required')) {
+      return 'Personal phone number required';
+    }
+
+    return this.surname.hasError('phoneNumber') ? 'Not a valid personal phone number' : '';
+  }
+  //Company Name
+  companyName = new FormControl('', [Validators.required]);
+
+  getErrorMessageCompanyName() {
+    if (this.companyName.hasError('required')) {
+      return 'Company name required';
+    }
+
+    return this.companyName.hasError('companyName') ? 'Not a valid company name' : '';
+  }
+  //Company Phone Number
+  companyNumber = new FormControl('', [Validators.required]);
+
+  getErrorMessageCompanyNumber() {
+    if (this.companyNumber.hasError('required')) {
+      return 'Company phone number required';
+    }
+
+    return this.companyNumber.hasError('companyNumber') ? 'Not a valid company phone number' : '';
   }
 }
