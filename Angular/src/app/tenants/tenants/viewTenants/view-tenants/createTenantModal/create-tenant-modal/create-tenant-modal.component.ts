e99b68116d9cb34bb5e15d4f8eded2d7cfcc2e9a
u05038015 <102801806+u05038015@users.noreply.github.com>
2023-08-20 19:48:17 +0200

    Tenant Validation

--- Angular/src/app/tenants/tenants/viewTenants/view-tenants/createTenantModal/create-tenant-modal/create-tenant-modal.component.ts
+++ Angular/src/app/tenants/tenants/viewTenants/view-tenants/createTenantModal/create-tenant-modal/create-tenant-modal.component.ts
@@ -19,6 +19,13 @@ export class CreateTenantModalComponent implements OnInit {
   editorRole: boolean = false;
   viewerRole: boolean = false;
 
+  errorMessages = {
+    required: 'This field is required.',
+    email: 'Please enter a valid email address.',
+    maxlength: 'This field cannot exceed 50 characters.',
+    pattern: 'Invalid input. Please check the format.',
+  };
+
   tenantModel: UserTenant = {
     username: "",
     password : "",
@@ -26,10 +33,12 @@ export class CreateTenantModalComponent implements OnInit {
     name : "",
     surname : "",
     phoneNumber : "",
-  companyName : "",
-  companyNumber : "",
-  profilePhoto : "",
+    companyName : "",
+    companyNumber : "",
+    profilePhoto : "",
   }
+
+  tenantForm!: FormGroup;
 
   constructor(
     private dialogRef: MatDialogRef<CreateTenantModalComponent>,
@@ -37,11 +46,22 @@ export class CreateTenantModalComponent implements OnInit {
     private tenantService : TenantService,
     private userService : UserService,
     private router : Router
-    ) {  }
+    ) {  
+      this.tenantForm = this.fb.group({
+        username: ['', [Validators.required, Validators.maxLength(50)]],
+        password: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        name: ['', [Validators.required, Validators.maxLength(50)]],
+        surname: ['', [Validators.required, Validators.maxLength(50)]],
+        phoneNumber: ['', [Validators.required, Validators.pattern('[0-9]')]],
+        companyName: ['', [Validators.required, Validators.maxLength(50)]],
+        companyNumber: ['', [Validators.required, Validators.pattern('[0-9]')]],
+      });
+    }
 
   ngOnInit(): void {  }
 
-  CreateTenant() {
+  /*CreateTenant() {
     debugger;
     this.tenantService.createTenant(this.tenantModel).subscribe(
       (response) => {
@@ -56,7 +76,76 @@ export class CreateTenantModalComponent implements OnInit {
         location.reload();
       }
     );
+  }*/
+
+  showPassword = false;
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
   }
+
+  CreateTenant() {
+    debugger;
+  
+    if (this.tenantForm.valid) {
+      this.tenantService.createTenant(this.tenantForm.value).subscribe(
+        (response) => {
+          console.log('Tenant created successfully:', response);
+          this.dialogRef.close();
+          location.reload();
+        },
+        (error) => {
+          console.error('Error creating tenant:', error);
+          this.dialogRef.close();
+          location.reload();
+        }
+      );
+    } 
+    else {
+      this.tenantForm.markAllAsTouched();
+    }
+  }
+
+  /*CreateTenant() {
+    debugger;
+  
+    if (this.tenantForm.valid) {
+      this.tenantService.createTenant(this.tenantForm.value).subscribe(
+        (response) => {
+          console.log('Tenant created successfully:', response);
+          this.dialogRef.close();
+          location.reload();
+        },
+        (error) => {
+          console.error('Error creating tenant:', error);
+          this.dialogRef.close();
+          location.reload();
+        }
+      );
+    } 
+    else {
+      this.tenantForm.markAllAsTouched();
+    }
+  }*/
+
+ /*CreateTenant() {
+    debugger;
+
+    if (this.tenantForm.valid) {
+      this.tenantService.createTenant(this.tenantForm.value).subscribe(
+        (response) => {
+          console.log('Tenant created successfully:', response);
+          this.dialogRef.close();
+          location.reload();
+        },
+        (error) => {
+          console.error('Error creating tenant:', error);
+          this.dialogRef.close();
+          location.reload();
+        }
+      );
+    }
+  }*/
 
   closeModal() {
     this.dialogRef.close();
