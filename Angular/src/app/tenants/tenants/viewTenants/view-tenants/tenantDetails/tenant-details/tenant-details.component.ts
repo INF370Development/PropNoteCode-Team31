901ce5742270beb9826f5664c78e28db2f5d2936
u05038015 <102801806+u05038015@users.noreply.github.com>
2023-08-21 03:51:15 +0200

    Load Details

--- Angular/src/app/tenants/tenants/viewTenants/view-tenants/tenantDetails/tenant-details/tenant-details.component.ts
+++ Angular/src/app/tenants/tenants/viewTenants/view-tenants/tenantDetails/tenant-details/tenant-details.component.ts
@@ -28,7 +28,7 @@ NgModule({
 
 export class TenantDetailsComponent implements AfterViewInit {
 
-  tenantDetail : Tenant = new Tenant();
+  /*tenantDetail : Tenant = new Tenant();
   //tenantDetail: Tenant[] = [];
 
   constructor(
@@ -48,4 +48,58 @@ export class TenantDetailsComponent implements AfterViewInit {
       console.log("Broker Result", result);
     });
   }*/
+
+  tenantDetail : Tenant = new Tenant();
+
+  constructor(
+    public dialog: MatDialog, 
+    private _tenantService: TenantService,  
+    private route:ActivatedRoute) {
+    console.log("tenant details", Tenant)
+  }
+
+  ngAfterViewInit(): void {
+    //this.loadTenant();
+  }
+
+  /*loadTenant()
+  {
+    this._tenantService.getTenant(this.route.snapshot.params['id']).subscribe((result) =>
+    {
+      this.tenantDetail = result
+      console.log("Tenant Result", result)
+    });
+  }*/
+  /*loadTenant() {
+    this._tenantService.getTenant(this.route.snapshot.params['id']).subscribe((result: Tenant | any) => {
+      this.tenantDetail = result as Tenant;
+  
+      if (this.isTenant(result)) {
+        this.tenantDetail = result;
+      } else {
+        console.error("Invalid Tenant object:", result);
+      }
+      console.log("Tenant Result", result);
+    });
+  }*/ 
+  /*loadTenant() {
+    const id = this.route.snapshot.params['id'];
+    this._tenantService.getTenant(id, null).subscribe((result: Tenant | any) => {
+      this.tenantDetail = result as Tenant;
+      console.log("Tenant Result", result);
+    });
+  }*/
+
+  /*loadTenant() {
+    const id = this.route.snapshot.params['id'];
+    this._tenantService.getTenant(tenantID).subscribe((result: Tenant | any) => {
+      this.tenantDetail = result as Tenant;
+      console.log("Tenant Result", result);
+    });
+  }
+  
+  isTenant(obj: any): obj is Tenant {
+    return 'tenantID' in obj && 'userID' in obj && 'companyName' in obj && 'companyNumber' in obj;
+  }  */
+  
 }
