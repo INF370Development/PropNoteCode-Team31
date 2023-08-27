b3a944c21a973ea2c74effa91c2f2509bce4297a
u05038015 <102801806+u05038015@users.noreply.github.com>
2023-08-27 20:18:44 +0200

    IDK

--- Angular/src/app/contractors/contractors/contractorTree/contractor-tree/contractor-tree.component.ts
+++ Angular/src/app/contractors/contractors/contractorTree/contractor-tree/contractor-tree.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+/*import { MatTreeNestedDataSource } from '@angular/material/tree';
+import { NestedTreeControl } from '@angular/cdk/tree';*/
 
 @Component({
   selector: 'app-contractor-tree',
   templateUrl: './contractor-tree.component.html',
   styleUrls: ['./contractor-tree.component.scss']
 })
-export class ContractorTreeComponent {
+export class ContractorTreeComponent {  //AfterViewInit, OnInit{
+
+  /*reeControl = new NestedTreeControl<ContractorNode>(node => node.children);
+  treeDataSource = new MatTreeNestedDataSource<ContractorNode>();
+
+  ngOnInit(): void {
+    this._contractorService.getContractors().subscribe((contractors: any) => {
+      // Transform your flat contractor data into a hierarchical structure.
+      const contractorNodes: ContractorNode[] = contractors.map(contractor => ({
+        data: contractor,
+        children: [], // You may populate this if your data has a hierarchy
+      }));
+      this.treeDataSource.data = contractorNodes;
+    });
+  }
+
+  // Other methods...
+
+  hasChild = (_: number, node: ContractorNode) => !!node.children && node.children.length > 0;
+}
+
+interface ContractorNode {
+  data: Contractor;
+  children: ContractorNode[];*/
 
 }
