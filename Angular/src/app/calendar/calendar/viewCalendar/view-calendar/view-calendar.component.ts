41d52d615a4d228bf57ae08ce931f9a294f0c56c
u05038015 <102801806+u05038015@users.noreply.github.com>
2023-09-04 02:50:46 +0200

    Random stuff

--- Angular/src/app/calendar/calendar/viewCalendar/view-calendar/view-calendar.component.ts
+++ Angular/src/app/calendar/calendar/viewCalendar/view-calendar/view-calendar.component.ts
@@ -12,7 +12,22 @@ export class ViewCalendarComponent {
 
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin]
+    plugins: [dayGridPlugin],
+    themeSystem: 'bootstrap',
+    events: [
+      {
+        title: 'Iteration 7 Review',
+        start: '2023-09-11', // Event start date
+        end: '2023-09-11',    // Event end date
+        color: 'red',
+      },
+      {
+        title: 'Taking a break',
+        start: '2023-09-15',
+        end: '2023-09-20',
+        color : 'green'
+      },
+    ]
   };
 }
 
