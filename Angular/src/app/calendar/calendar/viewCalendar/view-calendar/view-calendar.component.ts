fe04098ed97ee9c82b642558ca985963cfe16bbe
u05038015 <102801806+u05038015@users.noreply.github.com>
2023-10-19 08:05:39 +0200

    why are you not working

--- Angular/src/app/calendar/calendar/viewCalendar/view-calendar/view-calendar.component.ts
+++ Angular/src/app/calendar/calendar/viewCalendar/view-calendar/view-calendar.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var gapi: any;
 
 @Component({
   selector: 'app-view-calendar',
   templateUrl: './view-calendar.component.html',
   styleUrls: ['./view-calendar.component.scss']
 })
-export class ViewCalendarComponent {
+export class ViewCalendarComponent implements OnInit{
+  private CLIENT_ID = '816728681069-sn07par3bmtfhhf00naiorndvf1u18bs.apps.googleusercontent.com';
+  private API_KEY = 'AIzaSyCu7TXNrUoKhUZR3joL725_q6YeHF-Jlo4';
 
+  calendarEvents: any[] = []; // Initialize an empty array to store calendar events
+
+  // ... Other properties and methods ...
+
+  ngOnInit(): void {
+    gapi.load('client', this.initClient.bind(this));
+  }
+
+  initClient() {
+    gapi.client.init({
+      apiKey: 'AIzaSyCu7TXNrUoKhUZR3joL725_q6YeHF-Jlo4',
+      clientId: '816728681069-sn07par3bmtfhhf00naiorndvf1u18bs.apps.googleusercontent.comD',
+      discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+      scope: 'https://www.googleapis.com/auth/calendar.readonly',
+    }).then(() => {
+      this.fetchCalendarEvents();
+    });
+  }
+
+  fetchCalendarEvents() {
+    // Use gapi.client to fetch Google Calendar events
+    gapi.client.calendar.events.list({
+      'calendarId': 'primary',
+      'timeMin': (new Date()).toISOString(),
+      'showDeleted': false,
+      'singleEvents': true,
+      'maxResults': 10,
+      'orderBy': 'startTime',
+    }).then((response) => {
+      this.calendarEvents = response.result.items;
+    });
+  }
 }
